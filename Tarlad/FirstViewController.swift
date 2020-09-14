309acbbd0237b3c6621d8239242522224d6f4baa
taras1808 <taras1808@gmail.com>
2020-09-14 16:53:16 +0300

    Initial Commit

---
+++ Tarlad/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  Tarlad
+//
+//  Created by Taras Kulyavets on 14.09.2020.
+//  Copyright © 2020 Tarlad. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Tarlad/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  Tarlad
+//
+//  Created by Taras Kulyavets on 14.09.2020.
+//  Copyright © 2020 Tarlad. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
